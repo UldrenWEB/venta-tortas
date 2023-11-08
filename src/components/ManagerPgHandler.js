1355aa916f74c968cc534acea3280d18c7a1188d
enmarcm <theenmanuel123@gmail.com>
2023-11-08 17:53:34 -0400

    jsdoc in ManagerPgHandler

--- src/components/ManagerPgHandler.js
+++ src/components/ManagerPgHandler.js
@@ -1,69 +1,103 @@
-'use strict'
-import PgHandler from "./PgHandler.js"
+"use strict";
+import PgHandler from "./PgHandler.js";
 
+/**
+ * Clase para manejar las consultas a la base de datos.
+ */
 class ManagerPgHandler {
-    constructor({ config, querys }) {
+  /**
+   * Crea una nueva instancia de ManagerPgHandler.
+   * @param {Object} options - Las opciones para crear la instancia.
+   * @param {Object} options.config - La configuración para el manejador de PostgreSQL.
+   * @param {Object} options.querys - Las consultas SQL que se pueden ejecutar.
+   */
+  constructor({ config, querys }) {
+    this.pgHandler = new PgHandler({ config });
 
-        this.pgHandler = new PgHandler({ config })
+    this.querys = querys;
+  }
 
-        this.querys = querys;
+  /**
+   * Ejecuta una consulta y devuelve una propiedad del resultado.
+   * @param {Object} options - Las opciones para la consulta.
+   * @param {string} options.key - La clave de la consulta a ejecutar.
+   * @param {Array} [options.params] - Los parámetros para la consulta.
+   * @param {string} options.prop - La propiedad del resultado a devolver.
+   * @returns {Promise<String>} - Una promesa que resuelve con la propiedad del resultado o un error.
+   */
+  returnByProp = async ({ key, params, prop }) => {
+    try {
+      let [result] = await this.pgHandler.executeQuery({
+        query: this.query[key],
+        params: params,
+      });
+
+      //Para comprobar:
+      console.log("Manager/ReturnByPropj ->", result[prop]);
+      return result[prop] ? result[prop] : false;
+    } catch (error) {
+      return { error };
     }
+  };
 
-    returnByProp = async ({ key, params = undefined, prop }) => {
-        try {
-            let [result] = await this.pgHandler.executeQuery({
-                query: this.query[key],
-                params: params
-            })
+  /**
+   * Ejecuta una consulta y devuelve si fue exitosa o no.
+   * Segun las filas que devuelva, se sabe si la consulta tuvo exito o no.
+   * @param {Object} options - Las opciones para la consulta.
+   * @param {string} options.key - La clave de la consulta a ejecutar.
+   * @param {Array} [options.params] - Los parámetros para la consulta.
+   * @returns {Promise<Boolean>} - Una promesa que resuelve con un booleano indicando si la consulta fue exitosa o un error.
+   */
+  execute = async ({ key, params }) => {
+    try {
+      let rowCount = await this.pgHandler.execute({
+        query: this.querys[key],
+        params: params,
+      });
 
-            //Para comprobar:
-            console.log('Manager/ReturnByPropj ->', result[prop]);
-            return result[prop] ? result[prop] : false
-        } catch (error) {
-            return { error }
-        }
+      return rowCount > 0;
+    } catch (error) {
+      return { error };
     }
+  };
 
-    //* Segun las filas que devuelva la consulta fue bien o mal
-    execute = async ({ key, params = undefined }) => {
-        try {
-            let rowCount = await this.pgHandler.execute({
-                query: this.querys[key],
-                params: params
-            })
+  /**
+   * Ejecuta una consulta y devuelve el resultado.
+   * @param {Object} options - Las opciones para la consulta.
+   * @param {string} options.key - La clave de la consulta a ejecutar.
+   * @param {Array} [options.params] - Los parámetros para la consulta.
+   * @returns {Promise<Array | Error>} - Una promesa que resuelve con el resultado de la consulta o un error.
+   */
+  executeQuery = async ({ key, params }) => {
+    try {
+      let result = await this.pgHandler.executeQuery({
+        query: this.querys[key],
+        params: params,
+      });
 
-            return rowCount > 0 ? true : false
-        } catch (error) {
-            return { error }
-        }
+      return result;
+    } catch (error) {
+      return { error };
     }
+  };
 
-    executeQuery = async ({ key, params = undefined }) => {
-        try {
-            let result = await this.pgHandler.executeQuery({
-                query: this.querys[key],
-                params: params
-            })
-
-            return result;
-        } catch (error) {
-            return { error }
-        }
+  /**
+   * Ejecuta una transacción con varias consultas.
+   * @param {Object} options - Las opciones para la transacción.
+   * @param {Array} [options.querys] - Las consultas a ejecutar en la transacción.
+   * @returns {Promise<Array | Error>} - Una promesa que resuelve con el resultado de la transacción o un error.
+   */
+  transaction = async ({ querys = [] }) => {
+    try {
+      const result = await this.pgHandler.transaction({
+        objQuerys: this.querys,
+        querys: querys,
+      });
+      return result;
+    } catch (error) {
+      return { error };
     }
-
-    transaction = async ({ querys = [] }) => {
-
-        try {
-            const result = await this.pgHandler.transaction({
-                objQuerys: this.querys,
-                querys: querys
-            })
-            return result;
-        } catch (error) {
-            return { error }
-        }
-    }
-
+  };
 }
 
-export default ManagerPgHandler
+export default ManagerPgHandler;
