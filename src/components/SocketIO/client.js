eab9636e1dd7ec1275158b121c9f07c843c0319e
UldrenWEB <uldrenmiguel33@gmail.com>
2023-11-13 02:08:57 -0400

    Se devolvieron cambios

--- src/components/SocketIO/client.js
+++ src/components/SocketIO/client.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 
 // import { io } from 'socket.io-client'
 //*Cuando se instacie se tienen que pasar los rooms y los namespace disponibles
@@ -8,6 +8,12 @@ class Client {
     this.url = url;
     this.io = io;
     this.namespace = 'myNamespace';
+    this.imgTag;
+    this.receivedEvent = {
+      room_message: this.#eventRoomMessage,
+      broadcast_message: this.#eventBroadcastMessage,
+      direct_message: this.#eventDirectMessage
+    }
   }
   //Crea el socket del cliente que se conecto
   createSocketClient = (user) => {
@@ -18,29 +24,30 @@ class Client {
 
       return socket;
     } catch (error) {
-      return { error: error.message };
+      return { error: error.message }
     }
-  };
+
+  }
   joinNamespace = (socket) => {
     try {
-      socket.emit("join_namespace", this.namespace);
+      socket.emit('join_namespace', this.namespace);
       return true;
     } catch (error) {
-      return { error: error.message };
+      return { error: error.message }
     }
-  };
+  }
 
   joinRoom = (socket, room) => {
     if (!this.rooms[room]) {
       return false;
     }
     try {
-      socket.emit("join_room", room);
+      socket.emit('join_room', room)
       return true;
     } catch (error) {
-      return { error: error.message };
+      return { error: error.message }
     }
-  };
+  }
 
   // Envía un mensaje a un room dentro de un namespace
   sendMessage = ({ socket, room, message }) => {
@@ -48,7 +55,7 @@ class Client {
       return false;
     }
     try {
-      socket.emit('message zone', { room, message });
+      socket.emit('message zone', { socket, room, message });
       return true;
     } catch (error) {
       return { error: error.message }
@@ -57,18 +64,17 @@ class Client {
 
   sendMessageBroadcast = ({ socket, message }) => {
     try {
-      socket.emit('general message', { namespace: this.namespace, message })
+      socket.emit('general message', { socket, namespace: this.namespace, message })
       return true;
     } catch (error) {
       return { error: error.message }
     }
   }
 
-
   // Envía un mensaje directo a un usuario
   sendDirectMessage = ({ socketEmit, user, message }) => {
     try {
-      socketEmit.emit('direct message', { user, message });
+      socketEmit.emit('direct message', { socketEmit, user, message });
       return true;
     } catch (error) {
       return { error: error.message }
@@ -76,10 +82,10 @@ class Client {
   }
 
   // Carga un archivo al servidor
-  #uploadFile = ({ user, file, destination, isImg }) => {
+  #uploadFile = ({ socket, user, file, destination, message, isImg }) => {
     const reader = new FileReader();
     reader.onload = (evt) => {
-      socket.emit('file', { file: evt.target.result, destination, user, isImg });
+      socket.emit('file', { socket, file: evt.target.result, destination, user, message, isImg });
       return true;
     };
     reader.onerror = (error) => {
@@ -89,89 +95,102 @@ class Client {
   }
 
   // Carga una imagen al servidor
-  loadImage = ({ socket, file, destination = this.namespace }) => {
+  loadImage = ({ socket, file, destination = this.namespace, message = 'image' }) => {
     this.#uploadFile({
       socket: socket,
       file: file,
       destination: destination,
       socket: socket,
-      typeFile: 'image'
+      typeFile: 'image',
+      message: message
     });
   }
 
   // Carga una nota de voz al servidor
-  loadVoiceNote = ({ socket, file, destination = this.namespace }) => {
+  loadVoiceNote = ({ socket, file, destination = this.namespace, message = 'voice' }) => {
     this.#uploadFile({
       socket: socket,
       file: file,
       destination: destination,
       socket: socket,
-      typeFile: 'voice'
+      typeFile: 'voice',
+      message: message
     });
   }
 
   //*Escucha los diferentes eventos del cliente
-  //Todo: Esto se deberia iterar para poder escuchar los eventos segun los usuario que se vayan conectando
   listenEvents = (socket) => {
+    /*
     socket.on('room message', this.#eventRoomMessage);
     socket.on('broadcast message', this.#eventBroadcastMessage);
     socket.on('direct message', this.#eventDirectMessage);
+    */
+    //!Iterando
+    const events = this.receivedEvent;
+    for (let key in events) {
+      socket.on(key, events[key])
+    }
   }
 
   //Este metodo devuelve el tag de la imagen para que sea mostrada en el chat
   #validateImage = (data) => {
     const { file, typeFile } = data;
-    if (typeFile === "image") {
-      console.log("Es una imagen");
-      const imgTag = `<img src="${file}">`;
+    if (typeFile === 'image') {
+      console.log('Es una imagen');
+      const imgTag = `<img src="${file}">`
       return imgTag;
-    } else if (typeFile === "voice") {
-      console.log("Es un audio");
+    } else if (typeFile === 'voice') {
+      console.log('Es un audio');
       //Manejo de audio
       return true;
     } else {
       return false;
     }
-  };
+  }
 
   #eventRoomMessage = (data) => {
     const isImg = this.#validateImage(data);
-    if (isImg) return isImg;
+    if (isImg) this.#setImgTag(isImg);
 
     console.log(`Mesaje recibido`, data);
     alert(data)
   }
   #eventBroadcastMessage = (data) => {
     const isImg = this.#validateImage();
-    if (isImg) return isImg;
+    if (isImg) this.#setImgTag(isImg);
 
     console.log('Mensage recibido por namespace, el mensaje es: ', data);
     alert(data)
   }
   #eventDirectMessage = (data) => {
     const isImg = this.#validateImage();
-    if (isImg) return isImg;
+    if (isImg) this.#setImgTag(isImg);
 
     console.log(`data`);
     alert(data)
   }
+  #setImgTag = (img) => {
+    this.imgTag = img
+  }
 
+  getImgTag = () => {
+    return this.imgTag;
+  }
 
   // Desplaza automáticamente el contenedor de chat hasta el final
   autoScroll = (container) => {
     container.scrollTop = container.scrollHeight;
-  };
+  }
 
   //incia la grabacion de audio
   startRecording = () => {
     return new Promise((resolve, reject) => {
-      navigator.mediaDevices
-        .getUserMedia({ audio: true })
-        .then((stream) => {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
           this.mediaRecorder = new MediaRecorder(stream);
           this.audioChunks = [];
 
-          this.mediaRecorder.addEventListener("dataavailable", (event) => {
+          this.mediaRecorder.addEventListener("dataavailable", event => {
             this.audioChunks.push(event.data);
           });
 
@@ -182,17 +201,17 @@ class Client {
             this.stopRecording();
           }, 120000);
         })
-        .catch((err) => {
+        .catch(err => {
           reject(err);
         });
     });
-  };
+  }
 
   // Detiene la grabación de audio
   stopRecording = () => {
     return new Promise((resolve, reject) => {
       if (!this.mediaRecorder) {
-        reject(new Error("No se está grabando audio"));
+        reject(new Error('No se está grabando audio'));
       } else {
         this.mediaRecorder.addEventListener("stop", () => {
           const audioBlob = new Blob(this.audioChunks);
@@ -207,7 +226,7 @@ class Client {
         this.mediaRecorder.stop();
       }
     });
-  };
+  }
 }
 
-export default Client;
+export default Client
